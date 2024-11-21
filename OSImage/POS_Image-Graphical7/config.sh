d8e448437943991667da91cb6952122e304fbbd4
Don Vosburg <donald.vosburg@suse.com>
2024-11-21 10:18:59 -0500

    Update config.sh

--- OSImage/POS_Image-Graphical7/config.sh
+++ OSImage/POS_Image-Graphical7/config.sh
@@ -125,7 +125,4 @@ fi
 # only for debugging
 #systemctl enable debug-shell.service
 
-# Make keyboard softlink if not there
-ln -s /usr/share/kbd/keymaps/xkb/cm.map.gz  /usr/share/kbd/keymaps/xkb/us.map.gz
-
 exit 0
